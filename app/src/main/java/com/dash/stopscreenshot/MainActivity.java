00785a17996ed5d61a51bf25fbf4e549c9dc78b2
Ashutosh <ashutosh.dash@quintuslabs.com>
2020-06-03 01:12:06 +0530

    first commit

---
+++ app/src/main/java/com/dash/stopscreenshot/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dash.stopscreenshot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE); //This will prevent Screenshots.
+        setContentView(R.layout.activity_main);
+    }
+}
